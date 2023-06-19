234438c164555e0c50d55e76624c93c5f6782835
ck_ngetich <kiprongetich3@gmail.com>
2023-06-19 08:37:03 -0700

    This is Intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - Entry point of the program
- * Return: 1 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19"\n);
-	retun (1);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n");
+	retun (0);
 }
