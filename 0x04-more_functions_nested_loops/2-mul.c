86807305eee3b1ae3fa25a4b71f51989ed30f751
ck_ngetich <kiprongetich3@gmail.com>
2023-06-22 14:10:42 -0700

    function that multiplies two integers.

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,7 +5,7 @@
  * @y: The second number to multiply with the second number
  * Return: The result of multiplication between x param and y param
  */
-int mul(int x,int y)
+int mul(int x, int y)
 {
 	return (x * y);
 }
