0e3fbb1e9e3f047fb79b1334bcbcddda9e93a7a2
ck_ngetich <kiprongetich3@gmail.com>
2023-06-22 15:08:25 -0700

    You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,6 +8,7 @@
 void print_square(int size)
 {
 	int x, y;
+
 	if (size <= 0)
 	{
 	_putchar('\n');
@@ -16,7 +17,7 @@ void print_square(int size)
 	{
 	for (x = 0; x < size; x++)
 	{
-	for (y = 0; y< size; y++)
+	for (y = 0; y < size; y++)
 	{
 	_putchar(35);
 	}
