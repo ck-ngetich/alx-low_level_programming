ccfa673ea3ba27be9567e467cd6b6410def75f5c
ck_ngetich <kiprongetich3@gmail.com>
2023-06-22 15:45:18 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,7 +10,7 @@ void print_triangle(int size)
 
 	if (size <= 0)
 	{
-	_putchar("\n");
+	_putchar('\n');
 	}
 	else
 	{
@@ -24,7 +24,7 @@ void print_triangle(int size)
 	{
 	_putchar(35);
 	}
-	_putchar("\n");
+	_putchar('\n');
 	}
 	}
 }
