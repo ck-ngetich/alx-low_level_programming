5a70310cc4037aa1906cf9a939701f01eaf83ee6
ck_ngetich <kiprongetich3@gmail.com>
2023-06-23 14:27:33 -0700

    Im a Champion

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+
 /**
- * main - Causes an infinite loop
- * @\\o/\n - The output of the loop
- * @i: The value to be tested
- * Return: 0
- */
+* main - causes an infinite loop
+* Return: 0
+*/
+
 int main(void)
 {
+int i;
 
-printf("infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
+
 i = 0;
 /*
- *  while (i < 10) - Commenting in the infinite loop
- *  {
- *  putchar(i);
- *  } - Commenting out the infinite loop
+ * while (i < 10)
+ * {
+ * putchar(i);
+ * }
  */
 printf("Infinite loop avoided! \\o/\n");
 
