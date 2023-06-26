8363d6d49bb7a06f28fcd9e8744c557dfa232a02
ck_ngetich <kiprongetich3@gmail.com>
2023-06-26 15:34:05 -0700

    on't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include <time.h>
 /**
- * main - Genarates random valid password for the 
+ * main - Genarates random valid password for the
  * program 101-crackme
  * Return: Always 0
  */
@@ -12,7 +12,7 @@ char passwd[84];
 int index = 0, sum = 0, half1, half2;
 
 srand(time(0));
-while(sum < 2722)
+while (sum < 2722)
 {
 passwd[index] = 33 + rand() % 94;
 sum += passwd [index++];
@@ -21,19 +21,19 @@ passwd[index] = '\0';
 if (sum != 2722)
 {
 	half1 = (sum - 2722) / 2;
-	half2 = (sum -2722)/ 2;
-if ((sum -2722) % 2 != 0)
+	half2 = (sum - 2722) / 2;
+if ((sum - 2722) % 2 != 0)
 	half1++;
 for (index = 0; passwd[index]; index++)
 {
-if(passwd[index] >= (33 + half1))
+if (passwd[index] >= (33 + half1))
 {
 passwd[index] -= half1;
 break;
 }
-for (index = 0;passwd[index]; index++)
+for (index = 0; passwd[index]; index++)
 {
-if(passwd[index] >= (33 + half2))
+if (passwd[index] >= (33 + half2))
 {
 passwd[index] -= half2;
 break;
@@ -42,4 +42,5 @@ break;
 }
 printf("%s", passwd);
 return (0);
+}
 }
