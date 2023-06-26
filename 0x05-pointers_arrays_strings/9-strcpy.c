46ac2fd21a3f5681fb8b1c28de5af8112f7cba00
ck_ngetich <kiprongetich3@gmail.com>
2023-06-26 14:56:27 -0700

    This is  strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -19,4 +19,15 @@ min *= -1;
 while (s[c] >= 48 && s[c] <= 57)
 {
 isi = 1;
-ni = (ni * 10) + s[c] - '0'; c++;
+ni = ((ni * 10) + s[c] - '0');
+c++;
+}
+if (isi == 1)
+{
+	break;
+}
+c++;
+}
+ni *= min;
+return (ni);
+}
