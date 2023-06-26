7b343dfc153550d6425da25d7668183a1be93def
ck_ngetich <kiprongetich3@gmail.com>
2023-06-26 14:20:43 -0700

    I can only go one way. I've not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,7 +5,7 @@
  * @s: The string to print.
  * Return: void
  */
-int print_rev(char *s)
+void print_rev(char *s)
 {
 int c = 0;
 while (s[c] != '\0')
@@ -14,7 +14,7 @@ c++;
 }
 for (c -= 1; c >= 0; c--)
 {
-_putchar(*s[c]);
+_putchar(s[c]);
 }
 _putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,11 +7,11 @@ void _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *x, int *y);
 int _strlen(char *s);
-void _puts(char *str);
-int  print_rev(char *s);
+void _puts(char *s);
+void  print_rev(char *s);
 void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
+void puts2(char *s);
+void puts_half(char *s);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
