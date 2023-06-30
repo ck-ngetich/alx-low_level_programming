0194e97e4fdb1fbfbd8971b6ac774d5929932d21
ck_ngetich <kiprongetich3@gmail.com>
2023-06-30 10:46:55 -0700

    Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * char*_strcpy - a function that cpies the string pointed
- to by src
+ * char*_strcpy - a function that cpies the string pointed to by src
  * @dest: copy to
  * @src: copy from
  * Return: string
@@ -11,7 +10,7 @@ char *_strcpy(char *dest, char *src)
 	int l = 0;
 	int x = 0;
 
-	while (*(src + 1) != '\00')
+	while (*(src + 1) != '\0')
 	{
 	l++;
 	}
