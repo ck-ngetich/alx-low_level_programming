13aa2cdb175091831babc8de67404c7a8ac7d1a1
ck_ngetich <kiprongetich3@gmail.com>
2023-07-05 12:32:05 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _puts_rev_recursion - Prints a string followed by a new line
- * @s : the string to print
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: the string to print
  * Return: Nothing
  */
 void _puts_recursion(char *s)
