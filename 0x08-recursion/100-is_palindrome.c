5ad7997b4bfffcde028735f46fd8ad5ad4bd2961
ck_ngetich <kiprongetich3@gmail.com>
2023-07-06 12:07:43 -0700

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,39 +1,39 @@
 #include "main.h"
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
 /**
- * _strlen_recursion - returns the length of a string
+ * is_palidrome - checks if a string is a palidrome
+ * @s: string to reverse
+ * Return: 1 if it is palidrome, 0 if not
+ */
+int is_palidrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+/*_strlen_recursion - returns the lengt of a string
  * @s: string to calculate the length of
- * Return: the length of a string
+ * Return: length of the string
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-		return (1 + _strlen_recursion(s + 1));
+	return (1 + _strlen_recursion(s + 1));
 }
-
 /**
- * comparator - compares each character of the string
+ * check_pal - Check the character recursively for palidrome
  * @s: string to check
- * @n1: smallest iterartor
- * @n2: biggest iterator
- * Return: .
+ * @i: iterator
+ * @len: length of the string
+ * Return: 1 if palidrome , 0 if not
  */
-int comparator(char *s, int n1, int n2)
+int check_pal(char *s, int i, int len)
 {
-	if (*(s + n1) != *(s + n2 - 1))
+	if (*(s + i) != *(s + len - 1))
 		return (0);
-	if  (n1 >= n2)
+	if (i >= len)
 		return (1);
-		return (0 + comparator(s, n1 + 1, n2 - 1));
-}
-/**
- * is_palidrome - detects is a string is a palidrome
- * @s: string to reverse
- * Return: 1 if s is a palidrome , 0 if not.
- */
-int is_palidrome(char *s)
-{
-	if (*s == '\0')
-		return (1);
-	return (comparator(s, 0, _strlen_recursion(s)));
+	return (check_pal(s, i + 1, len - 1));
 }
