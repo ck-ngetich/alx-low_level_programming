47bc0e0447e05a9325fbcf2f150ab8b4f297cd6f
ck_ngetich <kiprongetich3@gmail.com>
2023-07-21 12:51:54 -0700

    One woman's constant is another woman's variable

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_all - prints anything
+ * @format:list the type of argument passed
+ */
+void print_all(const char * const format, ...)
+{
+	int x = 0;
+	char *str, *sep = "";
+	va_list = list:wq
