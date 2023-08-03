2ef0908a376f84ade9de76c1a15498a98c69f975
ck_ngetich <kiprongetich3@gmail.com>
2023-08-03 13:25:56 -0700

    a function that returns the value of a bit at a given index.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,23 +7,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned int num, raise;
-        int size;
+	unsigned int num, raise;
+	int size;
 
-        if (b == NULL)
-                return (0);
+	if (b == NULL)
+		return (0);
 
-        for (size = 0; b[size]; size++)
-        {
-                if (b[size] != '0' && b[size] != '1')
-                        return (0);
-        }
+	for (size = 0; b[size]; size++)
+	{
+		if (b[size] != '0' && b[size] != '1')
+			return (0);
+	}
 
-        for (raise = 1, num = 0, size--; size >= 0; size--, raise *= 2)
-        {
-                if (b[size] == '1')
-                        size += raise;
-        }
+	for (raise = 1, num = 0, size--; size >= 0; size--, raise *= 2)
+	{
+		if (b[size] == '1')
+			size += raise;
+	}
 
-        return (num);
+	return (num);
 }
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_bit - a function to returns a value of a given index
+ * @index:  the value of the bit
+ * @n: number of checks
+ * Return: value at given index or -1 if an error occours
+ */
+int get_bit(unsigned long int n, unsigned int index)
+
+{
+	unsigned int x;
+
+	if (index > (sizeof(8) * 8))
+		return (-1);
+
+	for (x = 0; x < index; x++)
+		n = n >> 1;
+	return ((n & 1));
+}
