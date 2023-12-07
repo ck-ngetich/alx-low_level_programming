7850fabe8f69cbf996a0c4e09d706caf6b1a0653
ck-ngetich <kiprongetich3@gmail.com>
2023-12-07 00:20:44 -0800

    Its Print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: ptr to the head or start of the doubly linked list
+ * Return: numbers of node
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int track_node;
+	for (track_node = 0; h != NULL; track_node++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (track_node);
+}
